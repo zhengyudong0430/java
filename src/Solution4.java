18460b80e64dba854ca1d841c4c38af315274774
zhengyudong0430 <2238642137@qq.com>
2020-08-31 23:26:02 +0800

    说点什么呢

--- src/Solution4.java
+++ src/Solution4.java
@@ -8,9 +8,26 @@
     }
 }
 */
-public class Solution {
+public class Solution4 {
     public ListNode deleteDuplication(ListNode pHead)
     {
-
+        ListNode node = new ListNode(0);
+        ListNode cur = pHead;
+        ListNode tmp = node;
+        while (cur!=null) {
+            if(cur.next!=null&&cur.val==cur.next.val) {
+                while (cur.next!=null&&cur.val==cur.next.val) {
+                    cur = cur.next;
+                }
+                cur = cur.next;
+            }
+            else {
+                tmp.next = cur;
+                tmp = tmp.next;
+                cur = cur.next;
+            }
+        }
+        tmp.next=null;
+        return node.next;
     }
 }
